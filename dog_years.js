af4c3f495da48e1613b7d3b72eb1102ac03df4ae
FafaKwame <nutifafa.titiati@amalitech.org>
2021-10-23 18:36:59 +0000

    dog_years

---
+++ dog_years.js
@@ -0,0 +1,18 @@
+// set variable as my current age.
+const myAge = 10
+// set variable as early dog years.
+let earlyYears = 2
+earlyYears *= 10.5
+// subtracting 2 from my age gives the lateryears
+let laterYears = myAge - 2
+// accounting for the dog years using lateryears
+laterYears *= 4
+
+console.log(earlyYears)
+console.log(laterYears)
+
+const myAgeInDogYears = earlyYears + laterYears
+// my name
+const myName = 'FafaKwame'.toLowerCase()
+
+console.log(`my name is ${myName}. I am ${myAgeInDogYears} years old in dog years`)
